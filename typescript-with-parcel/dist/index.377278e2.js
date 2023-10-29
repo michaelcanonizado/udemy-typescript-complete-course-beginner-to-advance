6b53763addcfa61bb8fa6c8e5a9ba87174cc9435
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-10-29 21:17:55 +0800

    Classes recall

--- typescript-with-parcel/dist/index.377278e2.js
+++ typescript-with-parcel/dist/index.377278e2.js
@@ -1,2 +1,14 @@
+class Person {
+    constructor(name, email){
+        this.name = name;
+        this.email = email;
+        console.log(`Name: ${name} | Email: ${email}`);
+    }
+    greet() {
+        return `Hello! My name is ${this.name}`;
+    }
+}
+const person1 = new Person("Michael", "mike@gmail.com");
+console.log(person1.greet());
 
 //# sourceMappingURL=index.377278e2.js.map
---
+++ typescript-with-parcel/src/index.ts
@@ -0,0 +1,18 @@
+class Person {
+	name: string;
+	email: string;
+
+	constructor(name: string, email: string) {
+		this.name = name;
+		this.email = email;
+		console.log(`Name: ${name} | Email: ${email}`);
+	}
+
+	greet() {
+		return `Hello! My name is ${this.name}`;
+	}
+}
+
+const person1 = new Person('Michael', 'mike@gmail.com');
+
+console.log(person1.greet());
