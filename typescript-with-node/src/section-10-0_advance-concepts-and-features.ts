090e4acfe004fa568a50aa380e3625505f8ab9e0
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-11-02 18:06:22 +0800

    refactored 1.typecasting comment

--- typescript-with-node/src/section-10-0_advance-concepts-and-features.ts
+++ typescript-with-node/src/section-10-0_advance-concepts-and-features.ts
@@ -1,2 +1,2 @@
-// 1. typecasting
+// 1. typecasting - overriding the inferred type by using 'as' followed by the desire type at the end of the expression.
 let firstNameField = document.querySelector('#firstName')! as HTMLInputElement;
