bf89a7eea2d14e2056b46312cf1a388ecbc99c9a
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-11-03 23:36:35 +0800

    added parameter infer

--- typescript-with-node/src/section-10-4_inferring-functions.ts
+++ typescript-with-node/src/section-10-4_inferring-functions.ts
@@ -1,5 +1,5 @@
 //
-// INFERRING THE RETURN TYPE OF THE FUNCTION
+// INFERRING THE RETURN TYPE OF A FUNCTION
 //
 function returnString() {
 	return 'this is a string';
@@ -12,3 +12,28 @@ type FuncReturnType1 = FunctionReturnTypeChecker<typeof returnString>;
 
 // infers a NUMBER, because it is not a function
 type FuncReturnType2 = FunctionReturnTypeChecker<number>;
+
+//
+// INFERRING PARAMETERS OF A FUNCTION
+//
+function Person(name: string, age: number) {
+	return {
+		name,
+		age,
+	};
+}
+type InferFirstArgOfFunction<T> = T extends (arg1: infer Arg1, ...args: any[]) => any
+	? Arg1
+	: never;
+type InferSecondArgOfFunction<T> = T extends (
+	arg1: any,
+	arg2: infer Arg2,
+	...args: any[]
+) => any
+	? Arg2
+	: never;
+
+type FirstArgOfFunctionType = InferFirstArgOfFunction<typeof Person>;
+type SecondArgOfFunctionType = InferSecondArgOfFunction<typeof Person>;
+
+export {};
