06b27566a659fd2bfb5be9a8ac2cdbdbc3237955
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-11-03 23:29:23 +0800

    refactored comment

--- typescript-with-node/src/section-10-4_inferring-functions.ts
+++ typescript-with-node/src/section-10-4_inferring-functions.ts
@@ -1,7 +1,9 @@
+//
+// INFERRING THE RETURN TYPE OF THE FUNCTION
+//
 function returnString() {
 	return 'this is a string';
 }
-
 // Check what if the generic passed is a function, if it is a function, return the return type of that function; else return the type of in the generic
 type FunctionReturnTypeChecker<T> = T extends () => infer R ? R : T;
 
