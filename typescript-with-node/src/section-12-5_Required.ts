9c3e7abd22a3fff2b9ce3fc7126e62a2379a765a
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-11-06 00:04:59 +0800

    added Required utility type

---
+++ typescript-with-node/src/section-12-5_Required.ts
@@ -0,0 +1,11 @@
+interface User {
+	name?: string;
+	age?: number;
+	email?: string;
+	password?: string;
+}
+
+// Allows us to require properties of a type that have been set to optional. Usually paired with Pick utility type
+type RegisterUser = Required<Pick<User, 'email' | 'password'>>;
+
+export {};
