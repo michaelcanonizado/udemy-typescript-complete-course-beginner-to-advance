860d015b846fc4fc24b9c57e6d84e04ca786ae1e
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-12-25 14:22:28 +0800

    refactored

--- typescript-with-node/src/section-10-4_inferring-functions.ts
+++ typescript-with-node/src/section-10-4_inferring-functions.ts
@@ -4,7 +4,7 @@
 function returnString() {
 	return 'this is a string';
 }
-// Check what if the generic passed is a function, if it is a function, return the return type of that function; else return the type of in the generic
+// Check if the generic passed is a function, if it is a function, return the return type of that function; else return the type of in the generic
 type FunctionReturnTypeChecker<T> = T extends () => infer R ? R : T;
 
 // infers a STRING
--- typescript-with-node/src/section-12-0_Awaited.ts
+++ typescript-with-node/src/section-12-0_Awaited.ts
@@ -19,7 +19,7 @@ const promise: Promise<obj> = new Promise((res, rej) => {
 // Catch the type alias of the object from the promise, and set it to a custom type, to be used on the variable that will hold the response object. This is useful as it allows us to catch the structure of an response object from an API, or check for changes on the response object.
 type AwaitedType = Awaited<typeof promise>;
 
-let type: AwaitedType = {
+let user: AwaitedType = {
 	name: 'mike',
 	age: 19,
 	email: 'mike@example.com',
